0a3f4d030336de7f2ec0647228e7d39a268ae07a
Nekika <hugo.jahnke@gmail.com>
2024-10-13 18:51:11 +0200

    feat: created the Server module

---
+++ lib/tulipe/server.ex
@@ -0,0 +1,48 @@
+defmodule Tulipe.Server do
+  use GenServer
+
+  @default_config %{
+    state: [],
+    time_zone: "Etc/UTC"
+  }
+
+  def start_link(config \\ %{}) when is_map(config) do
+    GenServer.start_link(__MODULE__, Map.merge(@default_config, config), name: S)
+  end
+
+  def list(pid) do
+    list(pid, :all)
+  end
+
+  def list(pid, event) do
+    GenServer.call(pid, {:list, event})
+  end
+
+  def report(pid, event) do
+    GenServer.cast(pid, {:report, event})
+  end
+
+  @impl true
+  def init(config) do
+    with {:ok, _datetime} <- DateTime.now(config.time_zone) do
+      {:ok, config}
+    end
+  end
+
+  @impl true
+  def handle_call({:list, :all}, _from, %{state: state} = config) do
+    {:reply, state, config}
+  end
+
+  @impl true
+  def handle_call({:list, event}, _from, %{state: state} = config) do
+    events = Enum.filter(state, fn {e, _} -> e == event end)
+    {:reply, events, config}
+  end
+
+  @impl true
+  def handle_cast({:report, event}, %{state: state, time_zone: time_zone} = config) do
+    new_event = {event, DateTime.now!(time_zone)}
+    {:noreply, %{config | state: [new_event | state]}}
+  end
+end
