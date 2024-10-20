1a0bb00c54cf9c063fb75e19144a8d6744738f12
Nekika <hugo.jahnke@gmail.com>
2024-10-20 18:25:47 +0200

    feat/refactor: better logging, errors handling and cleaner serve loop

--- lib/tulipe_server.ex
+++ lib/tulipe_server.ex
@@ -10,45 +10,63 @@ defmodule TulipeServer do
     accept_forever(listener)
   end
 
+  defp log(socket, message) do
+    Logger.info("[#{inspect(socket)}] " <> message)
+  end
+
   defp accept_forever(listener) do
     with {:ok, socket} <- :gen_tcp.accept(listener) do
-      Logger.info("Accepted new connection")
+      log(socket, "Accepted new connection.")
       Task.start(fn -> serve_forever(socket) end)
       accept_forever(listener)
     end
   end
 
   defp serve_forever(socket) do
-    result =
-      with {:ok, packet} <- recv(socket),
-           {:ok, command} <- TulipeServer.Command.parse(packet),
-           do: TulipeServer.Command.run(command)
+    receive_forever(socket, fn packet ->
+      with {:ok, command} <- TulipeServer.Command.parse(packet),
+           {:ok, response} <- TulipeServer.Command.run(command) do
+        send_packet(response, socket)
+        :continue
+      else
+        {:error, :unknown_command} ->
+          send_packet("UNKNOWN COMMAND", socket)
+          :continue
 
-    send_result(result, socket)
-
-    serve_forever(socket)
+        {:error, :unsupported_event} ->
+          send_packet("UNSUPPORTED EVENT", socket)
+          :continue
+      end
+    end)
   end
 
-  defp recv(socket) do
-    with {:ok, packet} <- :gen_tcp.recv(socket, 0) do
-      Logger.info("Received packet from #{inspect(socket)}: #{inspect(packet)}")
-      {:ok, packet}
+  @spec receive_forever(socket, callback) :: :ok
+        when socket: :gen_tcp.socket(),
+             callback: (binary() -> :continue | {:stop, term()})
+  defp receive_forever(socket, callback) do
+    with {:ok, packet} <- receive_packet(socket),
+         :continue <- callback.(packet) do
+      receive_forever(socket, callback)
+    else
+      {:error, :closed} ->
+        log(socket, "Connection has been closed by remote peer.")
+
+      {:error, reason} ->
+        log(socket, "An error has ocurrend while trying to receive packet: #{inspect(reason)}.")
+
+      {:stop, reason} ->
+        log(socket, "Reception loop stopped by the callback: #{inspect(reason)}")
     end
   end
 
-  defp send_result({:ok, result}, socket) do
-    send_message(result, socket)
+  defp receive_packet(socket) do
+    with {:ok, packet} = result <- :gen_tcp.recv(socket, 0) do
+      log(socket, "Received packet: #{inspect(packet)}")
+      result
+    end
   end
 
-  defp send_result({:error, :unknown_command}, socket) do
-    send_message("UNKNOWN COMMAND", socket)
-  end
-
-  defp send_result({:error, :unsupported_event}, socket) do
-    send_message("UNSUPPORTED EVENT", socket)
-  end
-
-  defp send_message(message, socket) do
-    :gen_tcp.send(socket, message <> "\r\n")
+  defp send_packet(packet, socket) do
+    :gen_tcp.send(socket, packet <> "\n")
   end
 end
